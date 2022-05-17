b21d25fa4e57ee008c25f1dbdb2517de630ff0d9
Hennry Chan <dual.hennry.chan@gmail.com>
2022-05-17 15:26:43 -0500

    Mostrando otras listas de objetos 15/35

---
+++ AspNetCore_ProyectoEscuela/Views/Alumno/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetCore_ProyectoEscuela.Views.Alumno
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
